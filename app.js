5317af4406ac95cc8bca11200ddefdf5fcc07832
FikFikk <fikri225456@gmail.com>
2025-10-02 11:27:03 +0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const path = require("path");
+const WhatsAppService = require("./services/whatsappService");
+const logger = require("./logger");
+const { MessageMedia } = require("whatsapp-web.js");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// WhatsApp Service
+const waService = new WhatsAppService();
+
+app.get("/", (req, res) => {
+  if (waService.isAuthenticated()) {
+    res.render("index", { loggedIn: true });
+  } else {
+    res.render("login", { qr: waService.qrCode, loggedIn: false });
+  }
+});
+
+app.get("/logout", async (req, res) => {
+  await waService.logout();
+  res.redirect("/");
+});
+
+// API to get QR code (for AJAX polling)
+app.get("/qr", async (req, res) => {
+  res.json({ qr: waService.qrCode, waState: await waService.isConnected() });
+});
+
+app.post("/send-file", async (req, res) => {
+  try {
+    const { fileUrl, chatId, fileName } = req.body;
+    const file = await MessageMedia.fromUrl(fileUrl, {
+      mime: "application/pdf",
+      filename: `${fileName}.pdf`,
+    });
+    await waService.client.sendMessage(chatId, file);
+    res.status(200).json({ status: "send file success" });
+  } catch (error) {
+    logger.error(error, "Send message file error");
+    res.status(500).json({ status: "error", message: error.message });
+  }
+});
+
+app.get("/conversations", async (req, res) => {
+  try {
+    const { phone } = req.query;
+    const chat = await waService.client.getChats();
+    const conversation = await chat[0].fetchMessages({ limit: 20 });
+
+    return res.status(200).json({
+      status: "success",
+      conversation: conversation.reverse(),
+    });
+  } catch (error) {
+    return res.status(500).json({ status: "error", message: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  logger.info(`Server running on http://localhost:${PORT}`);
+});
---
+++ services/whatsappService.js
@@ -0,0 +1,145 @@
+const { Client, LocalAuth } = require("whatsapp-web.js");
+
+const qrcode = require("qrcode");
+const fsp = require("fs/promises");
+const logger = require("./../logger");
+
+class WhatsAppService {
+  constructor() {
+    this.qrCode = null;
+    this.sessionKey = "default";
+    this.restarting = false;
+    this.backoff = 2000;
+
+    this.client = new Client({
+      authStrategy: new LocalAuth({
+        clientId: this.sessionKey,
+        dataPath: "C:\\wwebjs_session",
+      }),
+      webVersionCache: {
+        type: "local",
+      },
+      puppeteer: {
+        headless: true,
+        executablePath:
+          "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+        args: [
+          "--no-sandbox",
+          "--disable-setuid-sandbox",
+          "--disable-dev-shm-usage",
+          "--disable-gpu",
+        ],
+      },
+      takeoverOnConflict: true,
+      takeoverTimeoutMs: 5000,
+      restartOnAuthFail: true,
+    });
+    this._init();
+  }
+
+  _init() {
+    try {
+      this.client.on("qr", (qr) => {
+        logger.info("QR code refreshed");
+        qrcode.toDataURL(qr, (err, url) => {
+          this.qrCode = url;
+        });
+      });
+      this.client.on("ready", () => {
+        logger.info("WhatsApp is ready!");
+        this.qrCode = null;
+      });
+      this.client.on("authenticated", () => {
+        logger.info("WhatsApp authenticated!");
+        this.qrCode = null;
+      });
+      this.client.on("auth_failure", () => {
+        logger.error("WhatsApp authentication failed!");
+        this.qrCode = null;
+      });
+      this.client.on("disconnected", async (reason) => {
+        logger.warn("WhatsApp disconnected:", reason);
+        await this.restart();
+      });
+      this.client.on("loading_screen", (percent, msg) => {
+        logger.info({ percent: percent, msg }, "LOADING")
+      });
+      this.client.on("change_state", (s) => logger.info(s, "STATE"));
+    } catch (error) {
+      logger.error(error, "Error initializing WhatsApp client");
+    }
+
+    this.client.initialize();
+    logger.info("WhatsApp client initializing...");
+  }
+
+  isAuthenticated() {
+    return this.qrCode === null;
+  }
+
+  async removeSession() {
+    try {
+      await fsp.rm(`C:\\wwebjs_session\\session-${this.sessionKey}`, {
+        recursive: true,
+        force: true,
+        maxRetries: 3,
+        retryDelay: 100,
+      });
+      logger.info("Session data removed");
+    } catch (error) {
+      logger.error(error.message, "Error removing session data");
+    }
+  }
+
+  async waitForBrowserClose(timeoutMs = 15000) {
+    try {
+      const proc = this.client?.pupBrowser?.process?.();
+      if (!proc) return;
+      await Promise.race([
+        new Promise((res) => proc.once("close", res)),
+        new Promise((res) => setTimeout(res, timeoutMs)),
+      ]);
+    } catch (error) {
+      logger.error(error.message, "Error waiting for browser close");
+    }
+  }
+
+  async restart() {
+    if (this.restarting) return;
+    this.restarting = true;
+    try {
+      logger.info("Restarting WhatsApp client...");
+      await this.client
+        ?.destroy()
+        .then(() => {
+          logger.info("Client destroyed successfully");
+        })
+        .catch((err) => {
+          logger.error(err, "Error during client destroy");
+        });
+      await this.waitForBrowserClose();
+      await this.removeSession();
+      setTimeout(() => {
+        this._init();
+        this.restarting = false;
+        this.backoff = 2000;
+      }, this.backoff);
+    } catch (error) {
+      logger.error(error.message, "Restarting error");
+      this.restarting = false;
+      this.backoff = Math.min(this.backoff * 2, 60000);
+      setTimeout(this.restart(), this.backoff);
+    }
+  }
+
+  async logout() {
+    await this.client?.logout();
+    await this.restart();
+  }
+
+  async isConnected() {
+    return (await this.client?.getState()) === "CONNECTED";
+  }
+}
+
+module.exports = WhatsAppService;
